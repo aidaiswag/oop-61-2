1d9becbfa76eacb21e4ef454cce66b11b53a58a6
aidaiswag <madambekovaaidai@gmail.com>
2025-12-11 09:43:00 +0600

    controlwork1 finished

--- controlwork/controlwork1.py
+++ controlwork/controlwork1.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 class Hero:
     def __init__(self, name, lvl, hp):
         self.name = name
@@ -39,6 +41,9 @@ class BankAccount:
     def get_bank_name(self):
         return self.bank_name
 
+    def get_bonus_for_level(self):
+        return self.hero.lvl * 10
+
     def __str__(self):
         return f"{self.hero.name} | Баланс: {self._balance} SOM"
 
@@ -47,16 +52,44 @@ class BankAccount:
             return self._balance + other._balance
         return "Ошибка! Нельзя сложить счета героев разных классов"
 
+    def __eq__(self, other):
+        return self.hero.name == other.hero.name and self.hero.lvl == other.hero.lvl and self.hero.hp == other.hero.hp
 
-mage = MageHero("Merlin", 80, 500, 150)
-warrior = WarriorHero("Conan", 50, 900, 0)
 
-acc1 = BankAccount(mage, 5000, "1234", "Simba")
-acc2 = BankAccount(warrior, 3000, "1111", "Simba")
+class SmsService(ABC):
+    @abstractmethod
+    def send_otp(self, phone):
+        pass
 
-print(mage.action())
+class KGSms(SmsService):
+    def send_otp(self, phone):
+        return f"<text>:Код: 1234</text><phone>{phone}</phone>"
+
+class RUSms(SmsService):
+    def send_otp(self, phone):
+        return {"text": "Код: 1234", "phone": phone}
+
+mage1 = MageHero("Merlin", 80, 500, 150)
+mage2 = MageHero("Merlin", 80, 500, 200)
+warrior = WarriorHero("Conan", 50, 900, 20)
+
+acc1 = BankAccount(mage1, 5000, "1234", "Simba")
+acc2 = BankAccount(mage2, 3000, "0000", "Simba")
+acc3 = BankAccount(warrior, 2500, "1111", "Simba")
+
+print(mage1.action())
 print(warrior.action())
+
 print(acc1)
-print(acc1.full_info())
-print(acc1 + acc1)     # работает
-print(acc1 + acc2)
+print(acc2)
+
+
+print("Банк:", acc1.get_bank_name())
+print("Бонус за уровень:", acc1.get_bonus_for_level(), "SOM")
+
+print("\n===Проверка eq===")
+print("Mage1==Mage2?", acc1==acc2)
+print("Mage1==Warrior?", acc1==acc3)
+
+sms = KGSms()
+print("\n", sms.send_otp("+996777123456"))
