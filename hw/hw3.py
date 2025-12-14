237ccdff1b407c4ac9cb8bbfd1c9f9fa7bc0c512
aidaiswag <madambekovaaidai@gmail.com>
2025-12-14 14:49:15 +0600

    update: hw3

---
+++ hw/hw3.py
@@ -0,0 +1,80 @@
+#Инкапсуляция
+# class Product:
+#         def __init__(self, name, price, discount):
+#             self.name = name
+#             self._price = price
+#             self.__discount = discount
+#
+#         def get_price(self):
+#             return self._price * (1-self.__discount/100)
+#
+#         def set_discount(self, percent):
+#             if percent <= 50:
+#                 self.__discount = percent
+#             return "Скидка больше 50%!"
+#
+#         def apply_extra_discount(self, secret_code):
+#             if secret_code == "VIP123":
+#                 self.__discount += 5
+#             return "Неверный код!"
+#
+# a = Product("DiStore Advent Calendar", 35000, 15)
+#
+# print("Цена со скидкой:", a.get_price())
+#
+# a.apply_extra_discount("VIP123")
+# print("Цена после VIP:", a.get_price())
+#
+# print(a.apply_extra_discount("HAPPY NEW YEAR"))
+# print("Цена итоговая:", a.get_price())
+
+
+ #Абстракция
+from abc import ABC, abstractmethod
+class PaymentMethod(ABC):
+    @abstractmethod
+    def pay(self, amount):
+        pass
+    @abstractmethod
+    def refund(self,amount):
+        pass
+
+class CardPayment(PaymentMethod):
+    def pay(self, amount):
+        return f"Оплата картой: {amount}"
+    def refund(self,amount):
+        return f"Возврат на карту: {amount}"
+
+class CashPayment(PaymentMethod):
+    def pay(self, amount):
+        return f"Оплата наличными: {amount}"
+    def refund(self,amount):
+        return f"Возврат наличными: {amount}"
+
+class CryptoPayment(PaymentMethod):
+    def pay(self, amount):
+        return {"type": "crypto",
+                "amount": amount,
+                "currency": "USDT"
+                }
+    def refund(self, amount):
+        return {"type": "crypto",
+                "amount": amount,
+                "currency": "USDT"
+                }
+
+
+class PaymentProcessor:
+    def __init__(self, payment_method: PaymentMethod):
+        self.payment_method = payment_method
+
+    def process(self, amount):
+        result = self.payment_method.pay(amount)
+        print(result)
+
+processor = PaymentProcessor(CardPayment())
+processor.process(1000)
+processor = PaymentProcessor(CashPayment())
+processor.process(889)
+processor = PaymentProcessor(CryptoPayment())
+processor.process(6777)
--- lessons/lesson3.py
+++ lessons/lesson3.py
@@ -1,120 +1,167 @@
 # Инкапсуляция
+import random
+import string
+
 
 # public — обычные атрибуты
 # _protected — одно подчёркивание: «не трогай, это внутреннее»
 # __private — два подчёркивания: работает name-mangling (настоящее скрытие)
 
-import random
+# import random
+#
+# class BankAccount:
+#
+#     def __init__(self, name, balance, password):
+#         self.name = name
+#         self._balance = balance
+#         self.__password = password
 
-class BankAccount:
+#     def get_balance(self, password):
+#         if password == self.__password:
+#             return self._balance
+#         return 'не верный пароль'
+#
+#     def get_money(self, password, amount):
+#         if password == self.__password:
+#             self._balance -= amount
+#             return self._balance
+#         return 'не верный пароль'
+#
+#     def _random_password(self):
+#         return random.randint(100000, 999999)
+#
+#     def reset_password(self, old_password):
+#         if self.__password == old_password:
+#             new_pass = self._random_password()
+#             self.__password = new_pass
+#             return self.__password
+#         return 'не верный пароль'
+#
+# ardager = BankAccount("Ardager", 100, "Def2638")
+# # print(ardager.reset_password("Def2638"))
+#
+#
+#
+#
+# #Абстракция
+#
+# from abc import ABC, abstractmethod
+#
+#
+# # Абстрактный класс Animal
+# class Animal(ABC):
+#
+#     @abstractmethod
+#     def move(self):
+#         pass
+#
+#     @abstractmethod
+#     def voce(self):
+#         pass
+#
+# class Dog(Animal):
+#
+#     def move(self):
+#         return "Ходит"
+#
+#     def voce(self):
+#         return "Гав Гав"
+#
+# my_dog = Dog()
+#
+# class User:
+#
+#     def __init__(self, name, country):
+#         self.name = name
+#         self.country = country
+#
+# ardager2  =  User("Ardager", "KG")
+# ardager3 = User("Ardager", "RU")
+#
+#
+# class ABCSendOTP(ABC):
+#     @abstractmethod
+#     def send_otp(self, user):
+#         pass
+#
+# class KGSendOTP(ABCSendOTP):
+#     def send_otp(self, user):
+#         sms = f"<Text>1234</Text><PhoneNumber>123123</PhoneNumber>"
+#         print(sms)
+#
+# class RUSendOTP(ABCSendOTP):
+#     def send_otp(self, user):
+#         sms = {
+#             'text': "1234",
+#             'phoneNumber': "12312"
+#         }
+#         print(sms)
+#
+# kg = KGSendOTP()
+# ru = RUSendOTP()
+#
+# class Register:
+#     def __init__(self, country1, country2):
+#         self.countryKG = country1
+#         self.countryRU = country2
+#
+#     def register(self, user):
+#
+#         if user.country == "KG":
+#             self.countryKG.send_otp(user)
+#         elif user.country == "RU":
+#             self.countryRU.send_otp(user)
+#
+# register = Register(kg, ru)
+#
+# user_name = input("Ввудите имя")
+# user_country = input("Введите страну")
+# my_user = User(user_name, user_country)
+# register.register(my_user)
+#
+# # register.register(ardager3)
+# # register.register(ardager2)
 
-    def __init__(self, name, balance, password):
-        self.name = name
-        self._balance = balance
-        self.__password = password
-
-    def get_balance(self, password):
-        if password == self.__password:
-            return self._balance
-        return 'не верный пароль'
-
-    def get_money(self, password, amount):
-        if password == self.__password:
-            self._balance -= amount
-            return self._balance
-        return 'не верный пароль'
-
-    def _random_password(self):
-        return random.randint(100000, 999999)
-
-    def reset_password(self, old_password):
-        if self.__password == old_password:
-            new_pass = self._random_password()
-            self.__password = new_pass
-            return self.__password
-        return 'не верный пароль'
-
-ardager = BankAccount("Ardager", 100, "Def2638")
-# print(ardager.reset_password("Def2638"))
 
 
 
 
-#Абстракция
 
-from abc import ABC, abstractmethod
+# class BankAccount:
+#     def __init__(self, login, balance, password):
+#         self.login = login
+#         self._balance = balance
+#         self.__password = password
+#
+#     def get_balance(self, password):
+#         if password == self.__password:
+#             return self._balance
+#         return "wrong password"
+#
+#     def get_money(self, password, amount):
+#         if password == self.__password:
+#             self._balance -= amount
+#             return self._balance
+#         return "wrong password"
 
+#     def _random_password(self):
+#         return random.randint(1000000, 100000000)
+#
+#     def reset_password(self, old_password):
+#         if self.__password == old_password:
+#             new_password = self._random_password()
+#             self.__password = new_password
+#             return self.__password
+#
+# aidai = BankAccount("aidaiswag", 200000, "idiot2006")
+# print(aidai._BankAccount__password)
+#
+# print(aidai.login)
+# aidai.login = "bitch"
+# print(aidai.login)
+# print(aidai.get_money("idiot2006", 200000))
+# print(aidai.reset_password("idiot2006"))
+# print(aidai._BankAccount__password)
+# print(aidai.reset_password("idiot2006"))
+# print(aidai.reset_password("17206854"))
 
-# Абстрактный класс Animal
-class Animal(ABC):
-
-    @abstractmethod
-    def move(self):
-        pass
-
-    @abstractmethod
-    def voce(self):
-        pass
-
-class Dog(Animal):
-
-    def move(self):
-        return "Ходит"
-
-    def voce(self):
-        return "Гав Гав"
-
-my_dog = Dog()
-
-class User:
-
-    def __init__(self, name, country):
-        self.name = name
-        self.country = country
-
-ardager2  =  User("Ardager", "KG")
-ardager3 = User("Ardager", "RU")
-
-
-class ABCSendOTP(ABC):
-    @abstractmethod
-    def send_otp(self, user):
-        pass
-
-class KGSendOTP(ABCSendOTP):
-    def send_otp(self, user):
-        sms = f"<Text>1234</Text><PhoneNumber>123123</PhoneNumber>"
-        print(sms)
-
-class RUSendOTP(ABCSendOTP):
-    def send_otp(self, user):
-        sms = {
-            'text': "1234",
-            'phoneNumber': "12312"
-        }
-        print(sms)
-
-kg = KGSendOTP()
-ru = RUSendOTP()
-
-class Register:
-    def __init__(self, country1, country2):
-        self.countryKG = country1
-        self.countryRU = country2
-
-    def register(self, user):
-
-        if user.country == "KG":
-            self.countryKG.send_otp(user)
-        elif user.country == "RU":
-            self.countryRU.send_otp(user)
-
-register = Register(kg, ru)
-
-user_name = input("Ввудите имя")
-user_country = input("Введите страну")
-my_user = User(user_name, user_country)
-register.register(my_user)
-
-# register.register(ardager3)
-# register.register(ardager2)
